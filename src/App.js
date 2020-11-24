04d009363b8c5193e125a88a82a8774e3e94b815
doanhnguyen99 <doanh080199@gmail.com>
2020-11-25 01:18:26 +0700

    register ui component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+// import Navbar from './component/navbar/navbar'
+import Login from './component/login/login'
+import SignUp from "./component/signup/signup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { SubMenu } = Menu;
+
+class App extends React.Component {
+  render() {
+   
+    return (
+        // <Login/>
+        <SignUp />
+    );
+  }
 }
 
 export default App;
