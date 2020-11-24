7ac296bd767218bbdc429d952442c8e45cd35266
doanhnguyen99 <doanh080199@gmail.com>
2020-11-25 00:41:13 +0700

    login ui

---
+++ src/component/login/style.js
@@ -0,0 +1,34 @@
+// #components-form-demo-normal-login .login-form {
+//     max-width: 300px;
+//   }
+//   #components-form-demo-normal-login .login-form-forgot {
+//     float: right;
+//   }
+//   #components-form-demo-normal-login .ant-col-rtl .login-form-forgot {
+//     float: left;
+//   }
+//   #components-form-demo-normal-login .login-form-button {
+//     width: 100%;
+//   }
+
+  const style = {
+    loginForm :{
+      maxWidth: "300px",
+      marginLeft: "40%",
+      marginTop: "200px"
+    },
+
+    antColRtl: {
+      float: 'left'
+    },
+
+    loginFormForgot: {
+      float: 'right'
+    },
+
+    loginFormButton: {
+      width: '100%'
+    }
+  }
+
+  export default style;
